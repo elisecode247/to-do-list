23b3c7efb21d75a608b7c146bc8e72ccfc52affb
elisecode247 <elisecode247@gmail.com>
2025-12-12 20:20:54 -0800

    fix missing comma

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/to-do-list/'
+  base: '/to-do-list/',
   plugins: [
     react({
       babel: {
