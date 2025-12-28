09c3491111c52a72ae7986752633325635dd3838
elisecode247 <elisecode247@gmail.com>
2025-12-27 17:59:23 -0800

    update api url

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -3,4 +3,4 @@ export const ARCHIVED_KEY = 'archived-checklist-items';
 export type PersistedChecklistKey =
   | typeof ITEMS_KEY
   | typeof ARCHIVED_KEY;
-export const API_URL = 'https://demo-server-production-9fc2.up.railway.app/chore';
+export const API_URL = 'https://demo-server-production-9fc2.up.railway.app/chores';
