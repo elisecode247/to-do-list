839c01abcd1a0412c4f28214ca0f2b6852b7e8f6
elisecode247 <elisecode247@gmail.com>
2026-01-02 21:09:19 -0800

    add dark mode

--- src/checklist/Checklist.tsx
+++ src/checklist/Checklist.tsx
@@ -286,6 +286,7 @@ const Checklist: FC<ChecklistProps> = ({
                                 key={item.id}
                                 id={item.id}
                                 text={item.text}
+                                tags={item.tags}
                                 lastCompleted={item.lastCompleted}
                                 deleteItem={deleteItem}
                                 toggleChecked={toggleChecked}
