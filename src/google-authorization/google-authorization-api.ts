1f57281bd375a1e3a9cf2569e04bd8b62e372724
elisecode247 <elisecode247@gmail.com>
2026-02-02 23:48:27 -0800

    add google tasks to auth scope

--- src/google-authorization/google-authorization-api.ts
+++ src/google-authorization/google-authorization-api.ts
@@ -11,7 +11,10 @@ export const connectGoogleCalendar = async (googleClientId: string): Promise<voi
     }
     window.google.accounts.oauth2.initCodeClient({
         client_id: googleClientId,
-        scope: "https://www.googleapis.com/auth/calendar.readonly",
+        scope: [
+            "https://www.googleapis.com/auth/tasks",
+            "https://www.googleapis.com/auth/calendar.readonly"
+        ],
         ux_mode: "popup",
         callback: async ({ code }: GoogleAuthCodeResponse) => {
             await fetch("/auth/google/calendar", {
