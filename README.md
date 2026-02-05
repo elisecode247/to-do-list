590bb810698d533a8d08a2e38afdad03b29821a4
elisecode247 <elisecode247@gmail.com>
2026-02-05 13:19:11 -0800

    fix category select color

--- README.md
+++ README.md
@@ -23,4 +23,3 @@ Live: https://elisecode247.github.io/to-do-list/
 - Reset all env variables
 - update task to get only incomplete tasks to capture past pending tasks
 - fixed archived subtasks
-- fix category font color
--- src/category-select/CategorySelect.tsx
+++ src/category-select/CategorySelect.tsx
@@ -6,21 +6,25 @@ interface CategorySelectProps {
     isFilter?: boolean;
     selectedCategory: string;
     onChange: (value: string) => void;
+    theme?: 'light' | 'dark';
 }
 
 const CategorySelect = ({
     id,
     isFilter = false,
     selectedCategory,
-    onChange
+    onChange,
+    theme = 'light'
 }: CategorySelectProps) => {
 
     return (
-        <div className={`category-select-wrapper ${isFilter ? 'category-select-wrapper_filter' : ''}`}>
+        <div className={`category-select-wrapper ${isFilter ? 'category-select-wrapper_filter' : ''}
+            ${theme === 'light' ? " category-select-wrapper--light " : " category-select-wrapper--dark "}`}>
             {!isFilter && <label htmlFor={id}>New Task Category:</label>}
             <select
                 id={id}
-                className={`category-select ${isFilter ? 'category-select_filter' : ''}`}
+                className={`category-select ${isFilter ? 'category-select_filter' : ''}
+                ${theme === 'light' ? " category-select_filter--light " : " category-select_filter--dark "}`}
                 value={selectedCategory}
                 onChange={(e) => onChange(e.target.value)}
             >
