d8f1d7959cf4135c469b1f838bc2bee38c63eea7
elisecode247 <elisecode247@gmail.com>
2026-02-05 19:06:26 -0800

    fix patch url

--- src/google-authorization/use-google-calendar.ts
+++ src/google-authorization/use-google-calendar.ts
@@ -125,7 +125,7 @@ export function useCalendarIntegration() {
             if (!isAuthenticated) return;
 
             try {
-                await fetch(`${API_AUTH_URL}/google/calendar/${listId}/${taskId}`, {
+                await fetch(`${API_AUTH_URL}/google/calendar/tasks/${listId}/${taskId}`, {
                     method: "PATCH",
                     headers: await authHeaders(),
                     body: JSON.stringify({ completed: isCompleted })
