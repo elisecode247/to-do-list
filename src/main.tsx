f2eefdc2b76526b66d0cf6a4b427ad094d65543b
elisecode247 <elisecode247@gmail.com>
2026-04-19 08:30:44 -0700

    combine providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
-import { createRoot } from 'react-dom/client'
-import 'src/index.css'
+import { createRoot } from 'react-dom/client';
+import type { ComponentType, ReactNode } from 'react';
+import 'src/index.css';
 import App from 'app/App';
 import { ToastProvider } from 'src/toast/toast-provider';
 import { AuthenticationProvider } from 'src/authentication/authentication-provider';
@@ -9,20 +10,37 @@ import { DemoProvider } from 'src/pages/demo/demo-provider';
 import { DemoTaskProvider } from 'src/pages/demo/demo-task-provider';
 import { AppRouter } from 'src/router';
 
+type ProviderComponent = ComponentType<{ children: ReactNode }>;
+
+const combineComponents = (components: ProviderComponent[]): ProviderComponent => {
+  return components.reduce(
+    (AccumulatedComponents, CurrentComponent) => {
+      return ({ children }: { children: ReactNode }) => {
+        return (
+          <AccumulatedComponents>
+            <CurrentComponent>{children}</CurrentComponent>
+          </AccumulatedComponents>
+        );
+      };
+    },
+    ({ children }: { children: ReactNode }) => <>{children}</>,
+  );
+};
+
+const providers = [
+    AppRouter,
+    AuthenticationProvider,
+    GoogleCalendarProvider,
+    ToastProvider,
+    DemoProvider,
+    DemoTaskProvider,
+    TaskProvider,
+];
+
+const AppContextProvider = combineComponents(providers);
+
 createRoot(document.getElementById('app-root')!).render(
-    <AppRouter>
-        <AuthenticationProvider>
-            <GoogleCalendarProvider>
-                <ToastProvider>
-                    <DemoProvider>
-                        <DemoTaskProvider>
-                            <TaskProvider>
-                                <App />
-                            </TaskProvider>
-                        </DemoTaskProvider>
-                    </DemoProvider>
-                </ToastProvider>
-            </GoogleCalendarProvider>
-        </AuthenticationProvider >
-    </AppRouter>
-)
+    <AppContextProvider>
+        <App />
+    </AppContextProvider>
+);
