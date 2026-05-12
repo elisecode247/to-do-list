6bde975046a7d167a812a3be00fab5c7cdfde267
elisecode247 <elisecode247@gmail.com>
2026-05-12 13:26:30 -0700

    update api url

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,7 +1,7 @@
 const isLocal = import.meta.env.VITE_LOCAL === 'true';
 
-export const API_URL = !isLocal ? "https://demo-server-production-9fc2.up.railway.app" : import.meta.env.VITE_API_URL;
-export const API_AUTH_URL = !isLocal ? "https://demo-server-production-9fc2.up.railway.app/auth" : import.meta.env.VITE_API_AUTH_URL;
-export const API_CHORES_URL = !isLocal ? "https://demo-server-production-9fc2.up.railway.app/chores" : import.meta.env.VITE_API_CHORES_URL;
-export const API_JOURNAL_URL = !isLocal ? "https://demo-server-production-9fc2.up.railway.app/journal" : import.meta.env.VITE_API_JOURNAL_URL;
-export const API_REFRESH_URL = !isLocal ? "https://demo-server-production-9fc2.up.railway.app/auth/refresh" : import.meta.env.VITE_API_REFRESH_URL;
+export const API_URL = !isLocal ? "https://api.dailyresetlist.com" : import.meta.env.VITE_API_URL;
+export const API_AUTH_URL = !isLocal ? "https://api.dailyresetlist.com/auth" : import.meta.env.VITE_API_AUTH_URL;
+export const API_CHORES_URL = !isLocal ? "https://api.dailyresetlist.com/chores" : import.meta.env.VITE_API_CHORES_URL;
+export const API_JOURNAL_URL = !isLocal ? "https://api.dailyresetlist.com/journal" : import.meta.env.VITE_API_JOURNAL_URL;
+export const API_REFRESH_URL = !isLocal ? "https://api.dailyresetlist.com/auth/refresh" : import.meta.env.VITE_API_REFRESH_URL;
