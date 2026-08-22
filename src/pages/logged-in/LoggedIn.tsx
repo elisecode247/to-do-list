773cb19ac44a2b41a005cba9953bae053d51f046
elisecode247 <elisecode247@gmail.com>
2026-08-22 10:49:16 -0700

    rename file

--- src/pages/logged-in/LoggedIn.tsx
+++ src/pages/logged-in/LoggedIn.tsx
@@ -562,7 +562,7 @@ const LoggedIn: React.FC = () => {
                         className="feedback-button"
                         label="Report a bug"
                         ariaLabel="Report a bug"
-                        icon={<img src="./bug.svg" width={30} />}
+                        icon={<img src="./ladybug.svg" width={30} />}
                         showLabel={false}
                         isPriority={false}
                     />
